c4819fb5af9a0c5ecb0c7f3289580bf00d8888ab
Neto test <malcev.ion2017@gmail.com>
2023-09-25 08:02:27 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - 4 * a * c;
+  if (D > 0) {
+    let x1;
+    let x2;
+    x1 = (-b - Math.sqrt(D)) / (2 * a);
+    x2 = (-b + Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (D == 0) {
+    let x;
+    x = -b / (2 * a);
+    arr.push(x);
+  } else if (D < 0) {
+    return arr;
+  }
   return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let percentMonth = (percent * 0.01) / 12;
+  let creditBody = amount - contribution;
+  let payMonth = creditBody * (percentMonth / (Math.pow((1 + percentMonth), countMonths) - 1));
+  let allPay = (payMonth * countMonths).toFixed(2);
+  return allPay;
+
 }
