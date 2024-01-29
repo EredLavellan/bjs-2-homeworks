7d22ce10fc66e4eb68b307612b12892722900c8e
EredLavellan <124075026+EredLavellan@users.noreply.github.com>
2024-01-29 21:24:40 +0300

    Первая попытка

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,29 +1,40 @@
 function Student(name, gender, age) {
-    return {
-    name,
-    gender,
-    age,
-    marks: []
-  }
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
+
 
 Student.prototype.setSubject = function (subjectName) {
     return this.setSubject = subjectName;
-  
+
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks === undefined){
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.marks === undefined){
+        return 'Нет оценок';
+    } else {
+        return this.marks.reduce( ( a, v ) => a + v, 0 ) / this.marks.length;
+    }
+
 }
 
 Student.prototype.exclude = function (reason) {
   delete this.marks;
 delete this.subject;
 this.excluded = reason;
-  
+
 }
